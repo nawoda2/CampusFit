2ea42eb85b45f1d90875d1fa469309d452142ed9
nawoda2 <nawoda3@gmail.com>
2023-04-17 18:52:13 -0700

    first commit

---
+++ CampusFit/CampusFit/CampusFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusFitApp.swift
+//  CampusFit
+//
+//  Created by Nawoda Wijesooriya on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CampusFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusFit/CampusFit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CampusFit
+//
+//  Created by Nawoda Wijesooriya on 4/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
