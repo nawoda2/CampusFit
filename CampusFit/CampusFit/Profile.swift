a0441670861f744b5a8b6efb9b64e7089fcfcf4e
nawoda2 <nawoda3@gmail.com>
2023-04-21 11:39:47 -0700

    Added posts to the profile page.

--- CampusFit/CampusFit/Profile.swift
+++ CampusFit/CampusFit/Profile.swift
@@ -70,28 +70,33 @@ struct ProfileView: View {
             Post(
                 id: UUID(), caption: "Biking", image: "IMG_6427")
         ]
-        ScrollView {
-            ZStack {
-                VStack {
-                    HStack {
-                        Image("pic").resizable().frame(maxWidth: 100, maxHeight: 100).clipShape(Circle()).padding(.all).offset(x: 30)
-                        
-                        Image("setting").resizable().frame(maxWidth: 50, maxHeight: 50).offset(x: 90, y: -10)
-                    }
-                    Text("Name").fontWeight(.bold).font(.title3)
-                    Text("@Username").fontWeight(.bold).font(.subheadline)
-                    Text(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.").font(.caption).padding(.all).fontWeight(.light)
-                    
-                    LazyVGrid(columns: columns, alignment: .center) {
-                        ForEach(newPosts) { item in PostView(newPost: item)}.frame(maxWidth: 300, maxHeight: 300).border(.ultraThinMaterial)
+        NavigationStack {
+            ScrollView {
+                    VStack {
+                        HStack {
+                            Image("pic").resizable().frame(maxWidth: 100, maxHeight: 100).clipShape(Circle()).padding(.all)
                         }
-                    Spacer()
-                
-                
-                }
-                
-            }
-        }.background(LinearGradient(gradient: Gradient(colors: [yellow, blue]), startPoint: .top, endPoint: .bottom))
+                        Text("Name").fontWeight(.bold).font(.title3)
+                        Text("@Username").fontWeight(.bold).font(.subheadline)
+                        Text(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.").font(.caption).padding(.all).fontWeight(.light)
+                        
+                        LazyVGrid(columns: columns, alignment: .center) {
+                            ForEach(newPosts) { item in PostView(newPost: item)}.frame(maxWidth: 300, maxHeight: 300).border(.ultraThickMaterial).padding(.all, -3)
+                            }
+                        Spacer()
+                    
+                    
+                    }.toolbar {
+                        ToolbarItemGroup(placement: .navigationBarTrailing) {
+                            Button {
+                                SettingView()
+                            } label: {
+                                Label("Settings", systemImage: "gearshape")
+                            }
+                        }
+                    }
+            }.background(LinearGradient(gradient: Gradient(colors: [yellow, blue]), startPoint: .top, endPoint: .bottom))
+        }
     }
 }
 
@@ -101,6 +106,12 @@ struct PostView: View {
         VStack {
             Image(newPost.image).resizable().scaledToFit()
         }
+    }
+}
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
